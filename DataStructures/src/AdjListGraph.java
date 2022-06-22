47beaec63edee5719f904323c1b95a40b59e0288
tim <tfierek357@gmail.com>
2022-06-22 13:45:02 -0400

    implemented adjacency list graph and created unit tests

--- DataStructures/src/AdjListGraph.java
+++ DataStructures/src/AdjListGraph.java
@@ -1,18 +1,118 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class AdjListGraph<V> {
+public class AdjListGraph {
 
+	
 	private class Vertex{
-		private HashMap<Vertex, List<Vertex>> adjacentVertices;
-		private V data;
+		private String data;
 		
-		public Vertex(V data) {
+		public Vertex(String data) {
 			this.data = data;
-			adjacentVertices = new HashMap<>();
+		}
+		
+		@Override
+		public boolean equals(Object o) {
+			if(o == null) {
+				return false;
+			}
+			else if(this == o) {
+				return true;
+			}
+			else if((o instanceof Vertex) && (((Vertex) o).data.equals(this.data))) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		
+		@Override
+		public int hashCode() {
+			return data.hashCode();
 		}
 	}
 	
+	private int numVertices;
+	private int numEdges;
+	private HashMap<Vertex, List<Vertex>> adjacentVertices;
+
+	public AdjListGraph() {
+		adjacentVertices = new HashMap<>();
+		numVertices = 0;
+		numEdges = 0;
+	}
 	
+	public boolean containsVertex(String data) {
+		Vertex v = new Vertex(data);
+		return(adjacentVertices.containsKey(v));
+	}
+	
+	public boolean addVertex(String data) {
+		Vertex newVertex = new Vertex(data);
+		
+		if(!adjacentVertices.containsKey(newVertex)) {
+			adjacentVertices.put(newVertex, new ArrayList<>());
+			numVertices++;
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public boolean removeVertex(String data) {
+		Vertex vertex = new Vertex(data);
+		
+		if(adjacentVertices.containsKey(vertex)) {
+			adjacentVertices.values().stream().forEach(e -> e.remove(vertex));
+			adjacentVertices.remove(vertex);
+			numVertices--;
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public void addEdge(String data1, String data2) {
+		Vertex v1 = new Vertex(data1);
+		Vertex v2 = new Vertex(data2);
+		adjacentVertices.get(v1).add(v2);
+		if(adjacentVertices.get(v2).add(v1)) {
+			numEdges++;
+		}
+	}
+	
+	public void removeEdge(String data1, String data2) {
+		Vertex v1 = new Vertex(data1);
+	    Vertex v2 = new Vertex(data2);
+	    List<Vertex> edgesV1 = adjacentVertices.get(v1);
+	    List<Vertex> edgesV2 = adjacentVertices.get(v2);
+	    if (edgesV1 != null)
+	        edgesV1.remove(v2);
+	    if (edgesV2 != null)
+	        edgesV2.remove(v1);
+	    numEdges--;
+	}
+	
+	public boolean areConnected(String data1, String data2) {
+		Vertex v1 = new Vertex(data1);
+		Vertex v2 = new Vertex(data2);
+		
+		return(adjacentVertices.get(v1).contains(v2));
+	}
+	
+	public List<Vertex> getAdjVertices(String data){
+		Vertex v = new Vertex(data);
+		return adjacentVertices.get(v);
+	}
+	
+	public int getNumVertices() {
+		return numVertices;
+	}
+	
+	public int getNumEdges() {
+		return numEdges;
+	}
 	
 }
--- DataStructures/src/AdjListGraphTest.java
+++ DataStructures/src/AdjListGraphTest.java
@@ -5,8 +5,154 @@ import org.junit.jupiter.api.Test;
 class AdjListGraphTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testConstructor1() {
+		AdjListGraph testGraph = new AdjListGraph();
+		
+		assertNotNull(testGraph);
+	}
+	
+	@Test
+	void testConstructor2() {
+		AdjListGraph testGraph = new AdjListGraph();
+		int expected = 0;
+		int actual = testGraph.getNumEdges();
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testConstructor3() {
+		AdjListGraph testGraph = new AdjListGraph();
+		int expected = 0;
+		int actual = testGraph.getNumVertices();
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testAddVertex1() {
+		AdjListGraph testGraph = new AdjListGraph();
+		int expected = 2;
+		testGraph.addVertex("vertex1");
+		testGraph.addVertex("vertex2");
+		int actual = testGraph.getNumVertices();
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testAddVertex2() {
+		AdjListGraph testGraph = new AdjListGraph();
+		boolean expected = true;
+		testGraph.addVertex("vertex1");
+		testGraph.addVertex("vertex2");
+		boolean actual = testGraph.containsVertex("vertex2");
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testRemoveVertex1() {
+		AdjListGraph testGraph = new AdjListGraph();
+		boolean expected = false;
+		testGraph.addVertex("vertex1");
+		testGraph.addVertex("vertex2");
+		testGraph.removeVertex("vertex2");
+		boolean actual = testGraph.containsVertex("vertex2");
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testRemoveVertex2() {
+		AdjListGraph testGraph = new AdjListGraph();
+		boolean expected = false;
+		
+		boolean actual = testGraph.removeVertex("vertex2");
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testRemoveVertex3() {
+		AdjListGraph testGraph = new AdjListGraph();
+		boolean expected = true;
+		testGraph.addVertex("vertex1");
+
+		boolean actual = testGraph.removeVertex("vertex1");
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testAddEdge1() {
+		AdjListGraph testGraph = new AdjListGraph();
+		int expected = 0;
+		testGraph.addVertex("bob");
+		testGraph.addVertex("mike");
+		testGraph.addVertex("john");
+
+		int actual = testGraph.getNumEdges();
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testAddEdge2() {
+		AdjListGraph testGraph = new AdjListGraph();
+		int expected = 2;
+		testGraph.addVertex("bob");
+		testGraph.addVertex("mike");
+		testGraph.addVertex("john");
+		testGraph.addEdge("john", "bob");
+		testGraph.addEdge("john", "mike");
+
+		int actual = testGraph.getNumEdges();
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testRemoveEdge1() {
+		AdjListGraph testGraph = new AdjListGraph();
+		int expected = 0;
+		testGraph.addVertex("bob");
+		testGraph.addVertex("mike");
+		testGraph.addVertex("john");
+		testGraph.addEdge("john", "bob");
+		testGraph.removeEdge("john", "bob");
+
+		int actual = testGraph.getNumEdges();
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testAreConnected1() {
+		AdjListGraph testGraph = new AdjListGraph();
+		boolean expected = true;
+		testGraph.addVertex("bob");
+		testGraph.addVertex("mike");
+		testGraph.addVertex("john");
+		testGraph.addEdge("john", "bob");
+
+		boolean actual = testGraph.areConnected("john", "bob");
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testAreConnected2() {
+		AdjListGraph testGraph = new AdjListGraph();
+		boolean expected = false;
+		testGraph.addVertex("bob");
+		testGraph.addVertex("mike");
+		testGraph.addVertex("john");
+		testGraph.addEdge("john", "bob");
+
+		boolean actual = testGraph.areConnected("john", "mike");
+		
+		assertEquals(expected, actual);
 	}
 
 }
